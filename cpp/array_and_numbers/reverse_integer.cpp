ecc481d8e690f495a1795b3e18f77deb0d9fe261
Yuquan Shan <springshan13@gmail.com>
2018-08-03 18:33:15 -0400

    Fix int overflow.

--- cpp/array_and_numbers/reverse_integer.cpp
+++ cpp/array_and_numbers/reverse_integer.cpp
@@ -24,11 +24,16 @@ class ReverseInt
 public:
   int reverse(int x)
   {
-    if (x == -2147483648) {
-      return 0;
-    }
     int res = 0;
     while (x != 0) {
+      // overflow check...
+      if (res < INT_MIN / 10 || res > INT_MAX / 10) {
+        return 0;
+      }
+      if ((res < 0 && res * 10 < INT_MIN - x % 10) ||
+          (res > 0 && res * 10 > INT_MAX - x % 10)) {
+        return 0;
+      }
       res = res * 10 + x % 10;
       x /= 10;
     }
@@ -41,7 +46,7 @@ int main()
   ReverseInt test;
   int x = 123;
   cout << x << "->" << test.reverse(x) << endl;
-  x = -321;
+  x = 1534236469;
   cout << x << "->" << test.reverse(x) << endl;
   return 0;
 }
